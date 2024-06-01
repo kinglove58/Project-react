cf738351c047e09c3c94b9efbddb76bf4273a236
Techking <Kprincejah@gmail.com>
2024-06-01 11:40:46 +0100

    content just added to index file in constant

--- landingpage/src/constant/index.js
+++ landingpage/src/constant/index.js
@@ -1,3 +1,85 @@
-export {
-    
-}
+import {
+  facebook,
+  instagram,
+  twitter,
+  truckFast,
+  shieldTick,
+  support,
+} from "../assets/icons";
+
+import {
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  customer1,
+  customer2,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  shoe8,
+} from "../assets/images";
+
+import { star } from "../assets copy/icons";
+
+export const navLink = [
+  {
+    href: "#home",
+    label: "Home",
+  },
+  {
+    href: "#about",
+    label: "About",
+  },
+  {
+    href: "#products",
+    label: "Products",
+  },
+  {
+    href: "#contact-us",
+    label: "Contact Us",
+  },
+];
+
+export const shoe = [
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3,
+  },
+];
+
+export const statistics = [
+  { value: "1K+", label: "Brands" },
+  { value: "500+", label: "Shoes" },
+  { value: "250K+", label: "Customers" },
+];
+
+export const Products = [
+  { imgURL: shoe4, name: "Nike Air Jordan-01", price: "$299" },
+  {
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "$210.20",
+  },
+  {
+    imgURL: shoe6,
+    name: "Nike Air Jordan-100",
+    price: "$220.20",
+  },
+  {
+    imgURL: shoe7,
+    name: "Nike Air Jordan-001",
+    price: "230.20",
+  },
+];
